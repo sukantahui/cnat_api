ae2f0c2f5b638366abec6ac178c47d6f5c583f9a
sukantahui <huisukanta77@gmail.com>
2025-11-07 15:53:23 +0530

    custom respone added with BaseRequest3

--- app/Http/Resources/AdmissionResource.php
+++ app/Http/Resources/AdmissionResource.php
@@ -19,7 +19,9 @@ class AdmissionResource extends BaseResource
             'student' => new StudentResource($this->whenLoaded('student')),
             'course' => new CourseResource($this->whenLoaded('course')),
             'courseStatus' => new CourseStatusResource($this->whenLoaded('courseStatus')),
+            'courseFees' => $this->course_fees,
             'admissionDate' => $this->admission_date,
+            'completionDate' => $this->completion_date,
             'createdAt' => $this->created_at,
             'updatedAt' => $this->updated_at,
         ];
